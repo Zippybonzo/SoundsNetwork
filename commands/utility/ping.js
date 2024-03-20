bc6279ccd51367ba08211100ff59ec7f142e257e
Zippybonzo <90575488+Zippybonzo@users.noreply.github.com>
2024-03-20 18:07:36 +0000

    Update ping.js

--- commands/utility/ping.js
+++ commands/utility/ping.js
@@ -5,11 +5,11 @@ module.exports = {
         .setDescription('Check the bot ping.'),
     async execute(interaction) {
         //check uptime
-        const uptime = process.uptime() / 1440
+        const uptime = process.uptime() / 1440;
         const embed = new EmbedBuilder()
             .setColor(0x0099FF)
             .setTitle('Sounds Network')
-            .setDescription(`Running for: ${Math.round(process.uptime())}days.\n API Latency: ${interaction.client.ws.ping}ms.`)
+            .setDescription(`Running for: ${Math.round(process.uptime())} seconds.\n API Latency: ${interaction.client.ws.ping}ms.`)
         await interaction.reply({ embeds: [embed] });
     },
 };
