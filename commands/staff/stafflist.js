6efb35815be004402dcaa4587b19dfced468fe82
Zippybonzo <90575488+Zippybonzo@users.noreply.github.com>
2024-03-23 20:45:58 +0000

    Update stafflist.js
    oopsie that too

--- commands/staff/stafflist.js
+++ commands/staff/stafflist.js
@@ -22,7 +22,7 @@ module.exports = {
             });
 
             const staffNames = staffMembers.map(staff => staff.username).join(', ');
-            embed.addField(role, staffNames || 'No staff in this role');
+            embed.addFields(role, staffNames || 'No staff in this role');
         }
 
         await interaction.reply({ embeds: [embed] });
