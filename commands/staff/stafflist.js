9f3d16c7772217e742a4098e5962a43aa4908e9f
Zippybonzo <90575488+Zippybonzo@users.noreply.github.com>
2024-03-23 20:42:54 +0000

    add the staff list command

---
+++ commands/staff/stafflist.js
@@ -0,0 +1,30 @@
+const { SlashCommandBuilder, MessageEmbed } = require('discord.js');
+const { getPrismaClient } = require("../../prisma.js");
+const prisma = getPrismaClient();
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('stafflist')
+        .setDescription('List the server staff.'),
+    async execute(interaction) {
+        const roles = ['Moderator', 'Senior Moderator', 'Administrator', 'Senior Administrator'];
+        const embed = new MessageEmbed()
+            .setColor(0x0099FF)
+            .setTitle('Staff List');
+
+        for (const role of roles) {
+            const staffMembers = await prisma.staff.findMany({
+                where: {
+                    role: {
+                        equals: role,
+                    },
+                },
+            });
+
+            const staffNames = staffMembers.map(staff => staff.username).join(', ');
+            embed.addField(role, staffNames || 'No staff in this role');
+        }
+
+        await interaction.reply({ embeds: [embed] });
+    },
+};
